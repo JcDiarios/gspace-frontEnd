1d36d2d3e1ac8ed96468428ff5bf3436d260894d
JcDiarios <jcarlodiarios@gmail.com>
2024-02-01 23:53:44 +0800

    img

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import routes from "./routes"
+import { Routes, Route } from "react-router-dom"
+
+const App = () => {
+  return (
+    <Routes>
+      {routes.map((route, index) => {
+        return (
+          <Route 
+          key={index} 
+          path={route.path} 
+          element={route.element} 
+          exact 
+          />
+        );
+      })}
+    </Routes>
+  );
+}
+
+export default App
